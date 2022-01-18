482fa21a3e4ae7b73686f0a7248167c65e68aa5d
Kesa Sai Prasanth Kumar <40555740+KSPK542@users.noreply.github.com>
2022-01-17 19:17:24 -0600

    Update autograder.py

--- autograder.py
+++ autograder.py
@@ -1,18 +1,3 @@
-# autograder.py
-# -------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
-# imports from python standard library
 from __future__ import print_function
 import grading
 import imp
@@ -144,11 +129,6 @@ def readFile(path, root=""):
     "Read file from disk at specified path and return as string"
     with open(os.path.join(root, path), 'r') as handle:
         return handle.read()
-
-
-#######################################################################
-# Error Hint Map
-#######################################################################
 
 # TODO: use these
 ERROR_HINT_MAP = {
